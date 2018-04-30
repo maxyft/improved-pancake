2b9a108a39870143c823bab9e74378075a26aa5e
maxyft <maxyft@yandex.ru>
2018-04-30 03:35:57 +0300

    test

--- binanceWS/node-back/app/models/Ticker.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var tickerSchema = new Schema({
-	name: String,
-	rank: Number
-});
-var Ticker = mongoose.model('Ticker', tickerSchema);
-module.exports = Ticker;
